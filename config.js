bf8d37cb67adc79df001970f48b8423fd535d382
Zachalot <70214931+Zachalot@users.noreply.github.com>
2026-02-12 17:51:04 -0500

    Add initial configuration in config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  platform: 'github',
+  // autodiscover finds all repos your token can see
+  autodiscover: true, 
+  autodiscoverFilter: '*', 
+  // 'onboarding: false' allows the bot to run without opening a "Welcome" PR in every repo
+  onboarding: false,
+  requireConfig: 'optional',
+  // Recommended starting point
+  extends: ['config:best-practices'],
+};
